38f9255439246f88ea5d1eaea43f8bca2cfecb46
A00467069 <jerry.jebasingh@smu.ca>
2023-11-28 18:50:13 -0400

    Modified Dev

--- AzurePipeline React/azure/src/App.js
+++ AzurePipeline React/azure/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Prod 
+          This is Dev 
         </p>
       </header>
     </div>
